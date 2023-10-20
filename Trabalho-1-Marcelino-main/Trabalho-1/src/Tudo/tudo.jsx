fc24a6922fef16126984ece46727bbc5305308f4
TommasoRodriguesdf <davicasari1@gmail.com>
2023-10-20 17:09:47 -0400

    Salvando no LocalStorege agora

--- Trabalho-1-Marcelino-main/Trabalho-1/src/Tudo/tudo.jsx
+++ Trabalho-1-Marcelino-main/Trabalho-1/src/Tudo/tudo.jsx
@@ -1,19 +1,25 @@
-import {useState} from "react"
-import "./index.css"
-import { Link } from "react-router-dom"
-export default function Tudo (){
+import { useEffect, useState } from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
 
-    const [produto , setProduto] = useState("")
-    const [preco , setPreco] = useState("")
-    const [id , setId] = useState(1)
-    const [lista , setLista] = useState([])
+export default function Tudo() {
+  const listaLocalStorege = JSON.parse(localStorage.getItem("Lista"));
+  const [produto, setProduto] = useState("");
+  const [preco, setPreco] = useState("");
+  const [lista, setLista] = useState(listaLocalStorege || []);
+  const [id, setId] = useState(1);
 
-    const salvar = (e) => {
-        e.preventDefault(); setLista([...lista, { produto:produto , preco:preco, id:id}])  
-      setId( id +  1)
-    }
-    
- const removeProduto = (id) => {
+  useEffect(() => {
+    localStorage.setItem("Lista", JSON.stringify(lista));
+  }, [lista]);
+
+  const salvar = (e) => {
+    e.preventDefault();
+    setLista([...lista, { produto: produto, preco: preco, id: id }]);
+    setId(id + 1);
+  };
+
+  const removeProduto = (id) => {
     const novaLista = lista.filter((prod) => prod.id !== id);
     setLista(novaLista);
   };
@@ -22,14 +28,33 @@ export default function Tudo (){
     <div className="container">
       <h1 style={{ color: "#007bff" }}>Lista de Produtos</h1>
       <form onSubmit={salvar}>
-        <h2>Nome: <input value={produto} onChange={(e) => setProduto(e.target.value)} /></h2>
-        <h2>Preço: <input type="number" value={preco} onChange={(e) => setPreco(e.target.value)} /></h2>
-        <button style={{ backgroundColor: "#007bff", color: "#fff" }}>ADD</button>
+        <h2>
+          Nome:{" "}
+          <input value={produto} onChange={(e) => setProduto(e.target.value)} />
+        </h2>
+        <h2>
+          Preço:{" "}
+          <input
+            type="number"
+            value={preco}
+            onChange={(e) => setPreco(e.target.value)}
+          />
+        </h2>
+        <button style={{ backgroundColor: "#007bff", color: "#fff" }}>
+          ADD
+        </button>
       </form>
       {lista.map((prod) => (
         <div className="produtos" key={prod.id}>
-          <p>Nome: {prod.produto}, Preço: {prod.preco}, Id: {prod.id}</p>
-          <button onClick={() => removeProduto(prod.id)} style={{ backgroundColor: "#d9534f", color: "#fff" }}>Remover</button>
+          <p>
+            Nome: {prod.produto}, Preço: {prod.preco}, Id: {prod.id}
+          </p>
+          <button
+            onClick={() => removeProduto(prod.id)}
+            style={{ backgroundColor: "#d9534f", color: "#fff" }}
+          >
+            Remover
+          </button>
         </div>
       ))}
     </div>
